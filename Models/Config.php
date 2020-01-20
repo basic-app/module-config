93daf93e3d8c3f7e47d3c1f34896b04888460d65
basic-app <dev@basic-app.com>
2020-01-20 23:58:58 +0300

    n/a

---
+++ Models/Config.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace BasicApp\Config\Models;
+
+class Config extends \BasicApp\Core\Entity
+{
+}
---
+++ Models/ConfigModel.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace BasicApp\Config\Models;
+
+class ConfigModel extends \BasicApp\Core\Model
+{
+
+    protected $table = 'config';
+
+    protected $primaryKey = 'config_id';
+
+    protected $returnType = Config::class;
+
+    public function setValue(string $class, string $property, $value)
+    {
+        $row = $this->where(['config_class' => $class, 'config_property' => $property])->first();
+
+        if (!$row)
+        {
+            $row = new Config;
+
+            $row->config_class = $class;
+
+            $row->config_property = $property;
+
+            $row->config_value = $value;
+        }
+
+        $this->save($row);
+    }
+
+
+}
